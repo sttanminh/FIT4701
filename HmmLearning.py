2e934984e21cf521c4c369f3761aecb8aaa99715
sttanminh <75659577+sttanminh@users.noreply.github.com>
2023-03-19 22:55:13 +1100

    add hmm learm outline

---
+++ HmmLearning.py
@@ -0,0 +1,37 @@
+from hmmlearn import hmm
+import numpy as np
+
+
+
+#Load data
+
+
+
+
+#Define paremeter for HMM training ( hidden states, startProb , transmat, emissionprb, covariance_type ,n_iter )
+#3D pose data (frames,19,3), training and test data
+
+
+
+#LDA (feature extract) 
+features = extract_features(data)
+
+
+
+
+#Data quantization
+
+
+
+
+
+#Training hmm on feature vector
+
+
+
+
+#Recognizing data using hmm
+
+
+
+
